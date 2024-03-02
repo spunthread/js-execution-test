5ce7200b324ab782c288f003dad4af3b68cda3a4
spunthread <140069941+spunthread@users.noreply.github.com>
2024-03-02 15:11:49 +0530

    Update first.js

--- first.js
+++ first.js
@@ -4,10 +4,12 @@ queueMicrotask(() => console.log(2));
 
 setTimeout(() => console.log(3), 0);
 
-requestIdleCallback(() => () => console.log(4), { timeout: 0 });
+requestIdleCallback(() => console.log(4), { timeout: 0 });
 
 console.log(5);
 
 new Promise(() => console.log(6));
 
 (async () => console.log(7))();
+
+requestAnimationFrame(() => console.log(8));
